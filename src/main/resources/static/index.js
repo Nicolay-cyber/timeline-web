8761335401bcbff87f2e5000aa9f2dc9f1088482
Nikolay <dukarev1@gmail.com>
2024-05-13 22:43:10 +0200

    Added index.html and index.js

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,32 @@
+angular.module('timeline', []).controller('indexController', function ($scope, $http) {
+    const contextPath = 'http://localhost:8189/timeline/api/v1';
+
+    $scope.loadParameters = function () {
+        $http.get(contextPath + '/parameters')
+            .then(function (response) {
+                $scope.ParametersList = response.data;
+            });
+    };
+/*
+    $scope.deleteStudent = function (studentId) {
+        $http.get(contextPath + '/students/delete/' + studentId)
+            .then(function (response) {
+                $scope.loadStudents();
+            });
+    }
+
+    $scope.changeScore = function (studentId, delta) {
+        $http({
+            url: contextPath + '/students/change_score',
+            method: 'GET',
+            params: {
+                studentId: studentId,
+                delta: delta
+            }
+        }).then(function (response) {
+            $scope.loadStudents();
+        });
+    }*/
+
+    $scope.loadParameters();
+});
