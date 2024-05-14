b8f6035c28c3c1f5e51b1031a1df086335d6c73b
diukarev <dukurev1@gmail.com>
2024-05-14 12:04:57 +0200

    changed server pathway to make it available on another local PCs

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,5 +1,5 @@
 angular.module('timeline', []).controller('indexController', function ($scope, $http) {
-    const contextPath = 'http://localhost:8189/timeline/api/v1';
+    const contextPath = 'http://192.168.0.229:8189/timeline/api/v1'; // for office
 
     $scope.loadParameters = function () {
         $http.get(contextPath + '/parameters')
